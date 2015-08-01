762c716809b2abfee2aaafd75b17ae013cf831e1
susmitabhatta <bhattasusmita07@gmail.com>
2015-08-01 22:51:40 +0545

    project setup

---
+++ contact.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Welcome to James Buchanan Pub and Restaurant</title>
+<link rel="stylesheet" type="text/css" href="xres/css/style.css" />
+<link rel="icon" type="image/png" href="xres/images/favicon.png" />
+<link type="text/css" href="css/styles.css" rel="stylesheet" media="all" />
+</head>
+
+<body>
+<div id="wrapper">
+	<div id="header">
+    <h1></h1>
+        <ul id="mainnav">
+			<li><a href="index.php">Home</a></li>
+            <li><a href="routes.php">Routes</a></li>
+            <li><a href="location.php">location</a></li>
+            <li class="current"><a href="contact.php">Contact Us</a></li>
+    	</ul>
+	</div>
+    <div id="content">
+    	<div id="gallerycontainer">
+			<div class="portfolio-area" style="margin:0 auto; padding:140px 20px 20px 20px; width:820px;">	
+				<div id="contactleft">
+					Sudurpaschim Yatayat<br>
+(Sudurpaschim Yatayat, INCORPORATED)<br>
+Dhangadhi, Metro Manila<br>
+Phone:  +977 9848752856<br><br>
+
+Sudurpaschim Yatayat<br>
+(Sudurpaschim Yatayat, INCORPORATED)<br>
+kailali, Metro Manila<br>
+Sudurpaschim Yatayat, Manila<br>
+- (091) 525131; <br>
+Telephone No: +977 9848752856<br><br>
+
+
+Sudurpaschim Yatayat, Inc.<br>
+Location: Ratopul, Main Road <br><br>
+
+Contact Numbers:<br>
+Sudurpaschim Yatayat<br>
+-  +977 9848752856<br>
+				</div><br>
+				<div id="contactright">
+					<h3>Message Form</h3>
+					<form class="validate" action="messageexec.php" method="POST">
+                        <p>
+                            <label for="name" class="required label">Name:</label><br>
+                            <input id="name" class="contactform" type="text" name="name" />
+                        </p>
+                        <p>
+                            <label for="email" class="required label">Email:</label><br>
+                            <input id="email" class="contactform" placeholder="Example: susmita@gmail.com" type="text" name="email" />
+                        </p>
+                        <p>
+                            <label for="subject" class="required label">Subject:</label><br>
+                            <input id="subject" class="contactform" type="text" name="subject" />
+                        </p>
+                        <p>
+                            <label id="message-label" for="message" class="required label">Message:</label><br>
+                            <textarea id="message" class="contactform" name="message" cols="28" rows="5"></textarea>
+                        </p>
+                        <p>
+                            <label></label>
+                            <input class="contactform" id="submit-button" type="submit" name="Submit" value="Submit" style="height: 35px;" />
+                        </p>
+                    </form>
+				</div>
+               	<div class="column-clear"></div>
+            </div>
+			<div class="clearfix"></div>
+        </div>
+    </div>
+    
+
+
+
+<div id="footer">
+	<h4>+977 9848752856; <a href="contact-us.php">Sudhur pachim yetayat, Metro Manila  </a></h4>
+	<p>Hours of Operation;Mon - Sun: 10:00 am - 12:00 am</p>
+</div>
+
+</div>
+</body>
+</html>
